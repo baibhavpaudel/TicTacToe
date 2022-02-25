421f04306ab7b33da6e5b8f436709071b05744a1
Baibhav Paudel <baibhav.paudel@thamescollege.edu.np>
2022-02-25 10:50:19 +0545

    Manually working single player demo.

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
 # TODO#1: make a gameboard
-game = [[1, 2, 1],
-        [0, 2, 0],
-        [1, 2, 0]]
-
-
-# def gameboard(gamemap, player=0, row=0, col=0, just_display=False):
-#     def show_gamemap(gamemap):
-#         print('   0  1  2')
-#         for count, row in enumerate(gamemap):
-#             print(count, row)
-#
-#     try:
-#         if not just_display:
-#             gamemap[row][col] = player
-#             show_gamemap(gamemap)
-#             return gamemap
-#         else:
-#             show_gamemap(gamemap)
-#     except IndexError as e:
-#         print(f"That is not a valid position! Error: {e}.")
-#     except Exception as e:
-#         print(f"Something went wrong! Error: {e}.")
-#
-#
-# gameboard(game, just_display=True)
-
-# # TODO#2: move around the gameboard
-# # gameboard(game, player=1, row=1, col=1)
-#
-# for i in range(3):
-#     row_choice = int(input("Which row position would you like to play? (0 1 2):  "))
-#     col_choice = int(input("Which column position would you like to play? (0 1 2):  "))
-#
-#     game = gameboard(gamemap=game, player=1, row=row_choice, col=col_choice)
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
 
 
 # TODO#3: create logic for wins
@@ -41,7 +10,7 @@ def win(gamemap):
     for row in gamemap:
         if row.count(row[0]) == len(row) and row[0] != 0:
             print("The player wins horizontally!")
-            return
+            return True
 
     # Vertical win
     # makes a list of columns in the gameboard
@@ -49,22 +18,49 @@ def win(gamemap):
     for col in columns:
         if col.count(col[0]) == len(col) and col[0] != 0:
             print("The player wins vertically!")
-            return
+            return True
 
     # Diagonal win
     check = [game[row][col] for row, col in enumerate(range(len(game)))]
     if check.count(check[0]) == len(check) and check[0] != 0:
         print("The player wins diagonally! (\\)")
-        return
+        return True
 
     check = [game[row][col] for row, col in enumerate(reversed(range(len(game))))]
     if check.count(check[0]) == len(check) and check[0] != 0:
         print("The player wins diagonally! (/)")
-        return
+        return True
 
 
-print('   0  1  2')
-for count, row in enumerate(game):
-    print(count, row)
+def gameboard(gamemap, player=0, row=0, col=0, just_display=False):
+    def show_gamemap(gamemap):
+        print('   0  1  2')
+        for count, row in enumerate(gamemap):
+            print(count, row)
 
-win(gamemap=game)
+    try:
+        if not just_display:
+            gamemap[row][col] = player
+            show_gamemap(gamemap)
+            return gamemap
+        else:
+            show_gamemap(gamemap)
+    except IndexError as e:
+        print(f"That is not a valid position! Error: {e}.")
+    except Exception as e:
+        print(f"Something went wrong! Error: {e}.")
+
+
+gameboard(game, just_display=True)
+
+# TODO#2: move around the gameboard
+play = True
+while play:
+    row_choice = int(input("Which row position would you like to play? (0 1 2): "))
+    col_choice = int(input("Which column position would you like to play? (0 1 2): "))
+
+    game = gameboard(gamemap=game, player=1, row=row_choice, col=col_choice)
+    if win(game):
+        play = False
+
+
