863d10f17644162f0bc621bcd06b48d3e4ec4395
Baibhav Paudel <baibhav.paudel@thamescollege.edu.np>
2022-02-25 09:28:47 +0545

    Function to manipulate gameboard.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 # TODO#1: make a gameboard
 game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-print('   0  1  2')
-for count, row in enumerate(game):
-    print(count, row)
+
+
+def gameboard(gamemap, player=0, row=0, col=0, just_display=False):
+    def show_gamemap(gamemap):
+        print('   0  1  2')
+        for count, row in enumerate(gamemap):
+            print(count, row)
+
+    if not just_display:
+        gamemap[row][col] = player
+        show_gamemap(gamemap)
+    else:
+        show_gamemap(gamemap)
+
+
+gameboard(game, just_display=True)
 
 # TODO#2: move around the gameboard
-for ix in range(len(game)):
-    game[ix][ix] = 1
-
-print('   0  1  2')
-for count, row in enumerate(game):
-    print(count, row)
-
-
+gameboard(game, player=1, row=1, col=1)
 
 
 # TODO#3: create logic for wins
+
